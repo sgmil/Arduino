2f6ea034d7319458c3593ae630ee0b61a2c66d92
sgmil <smilheim@gmail.com>
2020-01-02 06:41:03 -0700

    templates added

---
+++ sketchbook/templates/files/ArduinoTemplate.ino
@@ -0,0 +1,21 @@
+{fileheader}
+#include "My_ESP8266_Functions.h"
+#include <Arduino.h>
+
+///////////////////////
+//Function Prototypes//
+///////////////////////
+
+/////////////
+//Variables//
+/////////////
+
+void setup(void) 
+{
+    
+}
+
+void loop(void)  
+{
+    
+}
