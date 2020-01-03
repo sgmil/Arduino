d31d1656a8e45e9c65ccb38061329f36b8854e8a
sgmil <smilheim@gmail.com>
2020-01-03 02:41:55 -0700

    change to sed to remove #define

--- scripts/ArduinoCompile.sh
+++ scripts/ArduinoCompile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 directory=$1
 file=$2
-board=$(sed -n '2p' < "$file" | cut -d' ' -f 2)
+#board=$(sed -n '2p' < "$file" | cut -d' ' -f 2)
+board=$(sed -n '2p' < "$file" | sed 's/#define board //g')
 echo "$board"
-IP=$(sed -n '3p' < "$file" | cut -d' ' -f 2)
+#IP=$(sed -n '3p' < "$file" | cut -d' ' -f 2)
+IP=$(sed -n '3p' < "$file" | sed 's/#define IP//g')
 echo "$IP"
+
 ~/bin/arduino-cli compile --fqbn "$board" "$directory"/"$file"
