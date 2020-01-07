55f4cc25701725bfb9c70a75ac5efad6b869820e
sgmil <smilheim@gmail.com>
2020-01-07 10:24:24 -0700

    put in line documentation

--- scripts/ArduinoUpload.sh
+++ scripts/ArduinoUpload.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-directory=$1
+  # arguments to pass to bash script
+directory=$1  # complete path
 file=$2
-
+  # extract board and port info from lines 2 and 3 of file and strip extras
 board=$(sed -n '2p' < "$file" | sed 's/#define BOARD //g' | sed 's/\"//g')
 echo "$board"
-IP_3=$(sed -n '3p' < "$file" | sed 's/#define IP_ADDRESS //g' | sed 's/\"//g')
-echo "$IP_3"
-
 port=$(sed -n '4p' < "$file" | sed 's/#define PORT //g' | sed 's/\"//g')
 echo "$port"
-
+  # upload command
 ~/bin/arduino-cli upload --port "$port" --fqbn "$board" "$directory"
