3bcb38f78da163c4343128ece4a03fac6c775b5f
sgmil <smilheim@gmail.com>
2020-01-07 09:58:22 -0700

    add line documentation

--- scripts/ArduinoCompile.sh
+++ scripts/ArduinoCompile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# pass directory path and filename as arguments when invoking bash script 
 directory=$1
 file=$2
-
+# extract board fqbn from file line 2 and strip out extras: 
 board=$(sed -n '2p' < "$file" | sed 's/#define BOARD //g' | sed 's/\"//g')
 echo "$board"
-IP=$(sed -n '3p' < "$file" | sed 's/#define IP_ADDRESS //g' | sed 's/\"//g')
-echo "$IP"
-
+# compile command:
 ~/bin/arduino-cli compile -v --fqbn "$board" "$directory"
