2b1ef4bec280fc38d119004b3d9c8b7c4bd5aa55
sgmil <smilheim@gmail.com>
2020-01-07 11:02:48 -0700

    Add line documentation

--- scripts/ArduinoOTAupload.sh
+++ scripts/ArduinoOTAupload.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-directory=$1
-file=$2
-stripped_file=$3
-
+  # arguments to pass to bash script:
+directory=$1      # complete path
+file=$2           # .ino
+stripped_file=$3  # no .ino
+  # extract board and port IP from lines 2 and 3 of file and strip extras
 board=$(sed -n '2p' < "$file" | sed 's/#define BOARD //g' | sed 's/\"//g')
 echo "$board"
 IP=$(sed -n '3p' < "$file" | sed 's/#define IP_ADDRESS //g' | sed 's/\"//g')
 echo "$IP"
+  # replace : with . and remove quotes from board name to make  filename
 boardfile=$(echo $board | sed s/:/./g | sed 's/\"//g')
 echo "$boardfile"
 echo "$directory"/"$stripped_file"."$boardfile".bin
-
+  #  OTA upload command
 python3 ~/SharedDocs/Arduino/scripts/ESPOTA.py -d -i "$IP" -f "$directory"/"$stripped_file"."$boardfile".bin
