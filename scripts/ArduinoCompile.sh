3e84ae8d11f635a21097f0d194155d4e0f9f852b
sgmil <smilheim@gmail.com>
2020-01-10 18:05:56 -0700

    cleanup

--- scripts/ArduinoCompile.sh
+++ scripts/ArduinoCompile.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# pass directory path and filename as arguments when invoking bash script 
+	# pass directory path and filename as arguments when invoking bash script 
 directory=$1
 file=$2
-# extract board fqbn from file line 2 and strip out extras: 
+	# extract board fqbn from file and strip out extras: 
 board=$(grep -i "#define BOARD" "$file" | sed 's/#define BOARD //g' | sed 's/\"//g')
 echo "$board"
-# compile command:
 IP=$(grep -i "#define IP_ADDRESS" "$file" | sed 's/#define IP_ADDRESS //g' | sed 's/\"//g')
 echo "$IP"
-
+	# compile command:
 ~/bin/arduino-cli compile -v --fqbn "$board" "$directory"
