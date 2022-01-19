b70432fdb2826a70c9cc78831449ae737a67b6c7
Stephen Milheim <smilheim@gmail.com>
2022-01-18 18:00:56 -0700

    file cleanup

---
+++ templates/files/ArduinoSketch.ino
@@ -0,0 +1,18 @@
+// {filename} // Copyright {date}  {developer}
+#define BOARD "avr:avr:uno"
+#define IP_ADDRESS ""
+#define PORT "/dev/ttyUSB0"
+/*
+ * Please make appropriate changes to above defines.
+ */
+// Function Prototypes
+
+// Variables
+
+void setup(void)
+{
+}
+void loop(void)
+{
+}
+
